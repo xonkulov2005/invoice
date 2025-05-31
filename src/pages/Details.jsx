7f59aa051fc0dc6263089418d66d79ab306a91e1
Ismingiz <email@example.com>
2025-05-31 11:51:19 +0500

    feat: refactor StatusBadge component and enhance Details page with invoice details display

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,4 +1,4 @@
-import { useNavigate, useParams } from "react-router-dom";
+import { data, useNavigate, useParams } from "react-router-dom";
 import { deleteById, getInvoice, updateById } from "../request";
 import { useState, useEffect } from "react";
 
@@ -89,7 +89,7 @@ export default function Details() {
     return <p>{error}</p>;
   }
   return (
-    <div className="py-5">
+    <div className="py-5 gap-6">
       <div className="base-container">
         <Card>
           <CardContent className="flex justify-between items-center">
@@ -149,6 +149,78 @@ export default function Details() {
               )}
             </div>
           </CardContent>
+        </Card>
+        <Card className="p-12 mt-6">
+          <div className="flex justify-between mb-5">
+            <div>
+              <h1 className="text-3xl font-[700]">#{invoice.id}</h1>
+              <p className="text-[#7E88C3]">{invoice.description}</p>
+            </div>
+            <div className="text-[#7E88C3] flex flex-col items-end">
+              <p>19 Union Terrace</p>
+              <p>London</p>
+              <p>E1 3EZ</p>
+              <p>United Kingdom</p>
+            </div>
+          </div>
+          <div className="flex justify-between">
+            <div>
+              <div className="mb-8 gap-3">
+                <span className="text-[#7E88C3]">Invoice Date</span>
+                <p className="text-xl font-bold">{invoice.createdAt}</p>
+              </div>
+              <div className="gap-3">
+                <span className="text-[#7E88C3]">Payment Due</span>
+                <p className="text-xl font-bold">{invoice.paymentDue}</p>
+              </div>
+            </div>
+            <div className="text-[#7E88C3]">
+              <p className="">Bill to</p>
+              <p className="text-xl text-[#0C0E16] font-bold">
+                {invoice.clientName}
+              </p>
+              <p>84 Church Way</p>
+              <p>Bradford </p>
+              <p>BD1 9PB</p>
+              <p>United Kingdom</p>
+            </div>
+            <div className="text-[#7E88C3]">
+              <p>Sent to</p>
+              <p className="text-xl text-[#0C0E16] font-bold">
+                {invoice.clientEmail}
+              </p>
+            </div>
+          </div>
+          <div className="px-8 pt-8 bg-[#F9FAFE] rounded-xl text-[#7E88C3]">
+            <div className="flex justify-between">
+              <div>
+                <p>Item Name</p>
+                <p className="font-bold text-[#0C0E16] my-8">Banner Design</p>
+                <p className="font-bold text-[#0C0E16]">Email Design</p>
+              </div>
+              <div>
+                <p>QTY.</p>
+                <p className="font-bold my-8">1</p>
+                <p className="font-bold">2</p>
+              </div>
+              <div>
+                <p>Price</p>
+                <p className="font-bold my-8">£ {invoice.total}</p>
+                <p className="font-bold">£ {invoice.total}</p>
+              </div>
+              <div>
+                <p>Total</p>
+                <p className="font-bold text-[#0C0E16] my-8">
+                  £ {invoice.total}
+                </p>
+                <p className="font-bold text-[#0C0E16]">£ {invoice.total}</p>
+              </div>
+            </div>
+          </div>
+          <div className="bg-[#373B53] text-white flex items-center justify-between rounded-xl p-8">
+            <p>Amount Due</p>
+            <h2 className="font-[700] text-3xl">£ {invoice.total}</h2>
+          </div>
         </Card>
       </div>
     </div>
